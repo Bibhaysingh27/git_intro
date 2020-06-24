b0c0a8e66eb1478f0356cfb15fef842c70cff29d
Bibhay@2001 <bibhaysingh2001@gmail.com>
2020-06-24 10:18:06 +0530

    Added a Fizzbuzz.py file to play a game of FizzBuzz

---
+++ Fizzbuzz.py
@@ -0,0 +1,11 @@
+n=int(input("Enter a number upto which we want to print:"))
+for i in range(1,n+1):
+    if i % 3 == 0 and i % 5 == 0:
+        print(str(i) + ": FizzBuzz")
+    elif i % 3 == 0:
+        print(str(i) + ": Fizz")
+    elif i % 5 == 0:
+        print(str(i) + ": Buzz")
+    else:
+        print(str(i) + ": ")
+    
